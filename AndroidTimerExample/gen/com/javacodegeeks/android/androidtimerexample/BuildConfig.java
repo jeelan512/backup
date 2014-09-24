43b0645335541e2b8c3a43e1d2a805aa9f36e597
jeelan <jeelan.cse@gmail.com>
2014-09-24 09:01:14 +0530

    Revert "Revert "Update Timer handler""
    This reverts commit 8209cb1d2047d3c99c285ac3a17443652297f944.

---
+++ AndroidTimerExample/gen/com/javacodegeeks/android/androidtimerexample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.javacodegeeks.android.androidtimerexample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
